ad557a261de8838e0c6627d4e2a34ef0f3f78e83
Alice2127 <aiokada2128@gmail.com>
2024-04-14 10:43:18 +0900

    不要なコメント削除

--- lib/hello_web/controllers/user_controller.ex
+++ lib/hello_web/controllers/user_controller.ex
@@ -2,9 +2,6 @@ defmodule HelloWeb.UserController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     text(conn, "Hello! This is the result of piyopiyoex #29")
-    # render(conn, :home, layout: false)
   end
 end
