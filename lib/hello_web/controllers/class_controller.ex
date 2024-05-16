76197ef4e11fc68756f141cb81e82409dc2c552a
Alice2127 <aiokada2128@gmail.com>
2024-05-17 00:21:50 +0900

    リダイレクト時のHTTPステータスを302から301に変更

--- lib/hello_web/controllers/class_controller.ex
+++ lib/hello_web/controllers/class_controller.ex
@@ -2,6 +2,8 @@ defmodule HelloWeb.ClassController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    redirect(conn, to: ~p"/users")
+    conn
+    |> put_status(301)
+    |> redirect(to: ~p"/users")
   end
 end
