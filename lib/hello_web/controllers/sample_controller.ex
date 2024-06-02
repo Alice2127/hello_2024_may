adacf224bb2a6287227105ca682c40f2f389c402
Alice2127 <aiokada2128@gmail.com>
2024-06-02 10:37:38 +0900

    sample_controllerのrender関数の引数を変えてみた

--- lib/hello_web/controllers/sample_controller.ex
+++ lib/hello_web/controllers/sample_controller.ex
@@ -4,6 +4,6 @@ defmodule HelloWeb.SampleController do
   def index(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :index, foo: "baz", bar: 123)
+    render(conn, :index, foo: "ほげ", bar: 987)
   end
 end
